074b74aff3663e081b0f3d0e8b62427dbf511b46
Nikola <84501234+colani-ops@users.noreply.github.com>
2026-03-05 16:29:09 +0100

    Update Program.cs

--- OpsCommand.Api/Program.cs
+++ OpsCommand.Api/Program.cs
@@ -126,12 +126,16 @@ if (!app.Environment.IsDevelopment())
     app.UseHttpsRedirection();
 }
 
+app.UseRouting();
+
 app.UseCors("FrontendPolicy");
 
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.Logger.LogInformation("CORS origins: {Origins}", string.Join(", ", corsOrigins));
 
 //User Seeding
 using (var scope = app.Services.CreateScope())
