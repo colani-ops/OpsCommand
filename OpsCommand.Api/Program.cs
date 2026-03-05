234ec61d228e80d0193474dd06acd72d5c4f6cb9
Nikola <84501234+colani-ops@users.noreply.github.com>
2026-03-05 16:19:02 +0100

    Update Program.cs

--- OpsCommand.Api/Program.cs
+++ OpsCommand.Api/Program.cs
@@ -118,11 +118,8 @@ if (corsOrigins.Length == 0)
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 if (!app.Environment.IsDevelopment())
 {
